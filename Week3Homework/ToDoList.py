f35706a296110406e5de6cafc76a8665a9fd58d9
RosianuRobert <94984223+RobertKeanu@users.noreply.github.com>
2023-04-27 18:33:06 +0300

    Next

---
+++ Week3Homework/ToDoList.py
@@ -0,0 +1,9 @@
+
+task = input("Taskul dorit este:")
+
+todos = [{"Task" : 'tema la mate',
+          "Data Limita" : '01.05.2023',
+          "Responsabil" : 'Ion Silviu',
+          "Categorie" : 'teme'}]
+
+print(list(todos))
